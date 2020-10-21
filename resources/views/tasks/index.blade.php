63d8ffd6cfd671eb591fedb40982ce0337422cc1
imanghafoori <imanghafoori1@gmail.com>
2020-10-21 21:06:35 +0330

    refactor

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -18,18 +18,18 @@
                 <div id="content">
                     <ul id="tabs" class="nav nav-tabs" data-tabs="tabs">
                         <li class="active"><a href="#all" data-toggle="tab"><span class="fa fa-tasks" aria-hidden="true"></span> <span class="hidden-xs">All</span></a></li>
-                        <li><a href="#incomplete" data-toggle="tab"><span class="fa fa-square-o" aria-hidden="true"></span> <span class="hidden-xs">Incomplete</span></a></li>
+                        <li><a href="#not_started" data-toggle="tab"><span class="fa fa-square-o" aria-hidden="true"></span> <span class="hidden-xs">Not started</span></a></li>
+                        <li><a href="#doing" data-toggle="tab"><span class="fa fa-check-square-o" aria-hidden="true"></span> <span class="hidden-xs">Doing...</span></a></li>
                         <li><a href="#done" data-toggle="tab"><span class="fa fa-check-square-o" aria-hidden="true"></span> <span class="hidden-xs">Complete</span></a></li>
-                        <li><a href="#failed" data-toggle="tab"><span class="fa fa-check-square-o" aria-hidden="true"></span> <span class="hidden-xs">failed</span></a></li>
-                        <li><a href="#doing" data-toggle="tab"><span class="fa fa-check-square-o" aria-hidden="true"></span> <span class="hidden-xs">doing</span></a></li>
-                        <li><a href="#skipped" data-toggle="tab"><span class="fa fa-check-square-o" aria-hidden="true"></span> <span class="hidden-xs">Skipped</span></a></li>
+                        <li><a href="#failed" data-toggle="tab"><span class="fa fa-square-o" aria-hidden="true"></span> <span class="hidden-xs">failed</span></a></li>
+                        <li><a href="#skipped" data-toggle="tab"><span class="fa fa-square-o" aria-hidden="true"></span> <span class="hidden-xs">Skipped</span></a></li>
                     </ul>
                     <div id="my-tab-content" class="tab-content">
 
-                        @widget('DefaultTaskList', ['tab' => 'incomplete', 'title' => 'Incomplete Tasks'])
+                        @widget('DefaultTaskList', ['tab' => 'not_started', 'title' => 'Not Started'])
                         @widget('TaskList', ['state' => null, 'tab' => 'all', 'title' => 'All Tasks', 'status' => 'active'])
                         @widget('TaskList', ['state' => 'done', 'tab' => 'done', 'title' => 'Done'])
-                        @widget('TaskList', ['state' => 'doing', 'tab' => 'doing', 'title' => 'Doing'])
+                        @widget('TaskList', ['state' => 'doing', 'tab' => 'doing', 'title' => 'Doing...'])
                         @widget('TaskList', ['state' => 'failed', 'tab' => 'failed', 'title' => 'Failed'])
                         @widget('TaskList', ['state' => 'skipped', 'tab' => 'skipped', 'title' => 'Skipped'])
 
